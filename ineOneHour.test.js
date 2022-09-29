46135fbc70d8959cac4c5e81e59de2b74f363eb0
João Ricardo Joaquim <joaorjoaquim@gmail.com>
2022-09-29 16:49:43 -0300

    testing date stuff

--- ineOneHour.test.js
+++ ineOneHour.test.js
@@ -2,7 +2,10 @@ const { inOneHour } = require('./inOneHour')
 
 describe('time function', () => {
     it('return the timestamp one hour ahead', () => {
+        const realDate = Date.now.bind(global.Date)     
         global.Date.now = () => 0
-        expect(inOneHour()).toBe(36000001)
+        expect(inOneHour()).toBe(3600000)
+        global.Date.now = realDate
+        console.log(global.Date.now())
     })
 })
