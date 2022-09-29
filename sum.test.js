efc969854f19091eb6627856a0ab38b3d5092769
João Ricardo Joaquim <joaorjoaquim@gmail.com>
2022-09-29 13:06:12 -0300

    before all and before each

--- sum.test.js
+++ sum.test.js
@@ -1,5 +1,19 @@
 const { sum } = require('./sum')
 
-it('sums 2 numbers', () => {
-    expect(sum(1,2)).toBe(3)
+describe('math functions', () => {
+    beforeAll(() => {
+        console.log('Before all')
+    })
+
+    beforeEach(() => {
+        console.log('before each')
+    })
+
+    it('sums 2 numbers', () => {
+        expect(sum(1,2)).toBe(3)
+    })
+
+    it('sums 2 numbers', () => {
+        expect(sum(1,2)).toBe(3)
+    })
 })
